eaefe26e4a115b17a7963b4feeb94439f7052811
unknown <cskeith01@gmail.com>
2022-02-23 11:02:20 +0100

    Finish exercise 2

--- junior.php
+++ junior.php
@@ -14,12 +14,25 @@ error_reporting(E_ALL);
 // Below we're defining a function, but it doesn't work when we run it.
 // Look at the error you get, read it and it should tell you the issue...,
 // sometimes, even your IDE can tell you what's wrong
-echo "Exercise 1 starts here:";
 
-function new_exercise()
+// echo "Exercise 1 starts here:";
+
+$x = 0; // Defined $x
+function new_exercise($x) // passing $
 {
-    $x = 1; // Defined $x
     $block = "<br/><hr/><br/><br/>Exercise $x starts here:<br/>"; // $x was not defined
     echo $block;
-}
-new_exercise(); // I had to call the function
+}; // close function with ;
+
+new_exercise(1); // I had to call the function
+
+
+new_exercise(2);
+// === Exercise 2 ===
+// Below we create a week array with all days of the week.
+// We then try to print the first day which is monday, execute the code and see what happens.
+
+$week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+$monday = $week[0]; // monday is on index 0 in $week
+
+echo $monday;
