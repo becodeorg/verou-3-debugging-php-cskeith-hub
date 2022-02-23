79332b44fc174129dae7d1360e0beb4b3492f232
unknown <cskeith01@gmail.com>
2022-02-23 11:34:51 +0100

    Finish Exercise 4

--- junior.php
+++ junior.php
@@ -43,3 +43,18 @@ new_exercise(3);
 
 $str = 'Debugged ! Also very fun';
 echo substr($str, 0, 24);// its only showing the 10 characters insted of 24 withs give the whole string
+
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it
+// Bonus: can you fix it with changing just one character? (hard)
+
+foreach($week as &$day) // i added & probley to call $day in the loop ?
+{
+    $day = substr($day, 0, -3);
+}
+print_r($week);
+
+
